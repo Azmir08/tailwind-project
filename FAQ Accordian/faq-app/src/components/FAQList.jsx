bb954a66b3693c3dee3d53e0f0cbdbe8cf762e5c
Azmir <azmirmahni08@gmail.com>
2025-12-02 18:05:38 +0800

    add title

--- FAQ Accordian/faq-app/src/components/FAQList.jsx
+++ FAQ Accordian/faq-app/src/components/FAQList.jsx
@@ -2,7 +2,12 @@ import FAQItem from "./FAQItem";
 
 const FAQList = () => {
   return (
-    <div>
+    <div className="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+      <div className="flex flex-col sm:flex-row justify-between items-center mb-8 gap-4">
+        <h2 className="text-3xl font-bold bg-linear-to-r from-indigo-600 via-purple-600 to-pink-600 text-transparent bg-clip-text text-center sm:text-left w-full sm:w-auto">
+          Frequently Asked Questions
+        </h2>
+      </div>
       <FAQItem />
     </div>
   );
